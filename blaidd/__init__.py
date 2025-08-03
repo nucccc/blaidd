f8d4ff20a2d1e5fe653d1dce9ef17ce5f0da4ced
nucccc <dan.dom.nuc@gmail.com>
2025-08-03 22:40:22 +0200

    got algorithm to work

--- blaidd/__init__.py
+++ blaidd/__init__.py
@@ -18,10 +18,56 @@ def color_series(
     )
 
     graph = _build_graph(label_centroids)
-    scale = colors_scale(len(graph.nodes))
+
+    assign_colors(graph)
+
+    return pl.Series(
+        name='color_graphed',
+        values=[graph.get_label_color(label) for label in df[c_col]]
+    )
 
 
-def colors_scale(n: int) -> list[float]:
+def assign_colors(graph: 'CentroidGraph'):
+    scale = Scale(len(graph.nodes))
+
+    # select the two farthest nodes
+    farthest1, farthest2 = graph.farthest_nodes()
+    farthest1.color_scale = scale.pop_first()
+    farthest2.color_scale = scale.pop_first()
+
+    while graph.n_uncolored() > 0:
+        closest_uncolored = graph.pick_closest_uncolored()
+        closest_uncolored.color_scale = scale.pop_farthest(
+            other_colors = closest_uncolored.pick_closest_colors()
+        )
+
+
+
+ScaleElem = int
+
+class Scale:
+
+    def __init__(self, n: int):
+        self.scale = colors_scale(n)
+
+    def pop_first(self) -> ScaleElem:
+        return self.scale.pop(0)
+
+    def pop_farthest(self, other_colors: list[ScaleElem]) -> ScaleElem:
+        max_score = None
+        farthest = None
+        for color in self.scale:
+            score = sum(abs(color - other_color) for other_color in other_colors)
+            if max_score is None or score > max_score:
+                max_score = score
+                farthest = color
+        
+        assert farthest is not None
+        self.scale.remove(farthest)
+        return farthest
+
+def colors_scale(n: int) -> list[ScaleElem]:
+    return list(range(n))
     step = 1.0 / (n - 1)
     return [i * step for i in range(n-1)] + [1.0]
 
@@ -33,7 +79,7 @@ def build_label_centroids(
     c_col: str,
 ) -> dict[Any, tuple[float, float]]:
     return {
-        label : (gdf[x_col].mean(), gdf[y_col].mean())
+        label[0] : (gdf[x_col].mean(), gdf[y_col].mean())
         for label, gdf in df.group_by(c_col)
     }
 
@@ -41,8 +87,26 @@ def build_label_centroids(
 @dataclass
 class CentroidNode:
     label: Any
+    # edges shall be ordered for closest to farthest
     edges: list[tuple[float, 'CentroidNode']] = field(default_factory=list)
-    color_scale: float | None = None
+    color_scale: ScaleElem | None = None
+
+    def colored(self) -> bool:
+        return not self.uncolored()
+    
+    def uncolored(self) -> bool:
+        return self.color_scale is None
+
+    def min_dist_to_colored(self) -> float:
+        return min(dist for dist, node in self.edges if node.colored())
+    
+    def pick_closest_colors(self, n_closest: int = 4) -> list[ScaleElem]:
+        closest_colors: list[ScaleElem] = list()
+        for dist, node in self.edges[:n_closest]:
+            if node.colored():
+                closest_colors.append(node.color_scale)
+
+        return closest_colors
 
 
 @dataclass
@@ -61,6 +125,32 @@ class CentroidGraph:
                     far2 = other_node
 
         return far1, far2
+    
+    def n_uncolored(self) -> int:
+        return sum(node.uncolored() for node in self.nodes)
+    
+    def pick_closest_uncolored(self) -> CentroidNode:
+        '''
+        picks the closest uncolored node to a colored one
+        '''
+        min_dist = None
+        closest_uncolored = None
+
+        for node in self.nodes:
+            if node.colored():
+                continue
+
+            dist = node.min_dist_to_colored()
+            if min_dist is None or dist < min_dist:
+                min_dist = dist
+                closest_uncolored = node
+
+        return closest_uncolored
+    
+    def get_label_color(self, label: Any) -> ScaleElem:
+        for node in self.nodes:
+            if node.label == label:
+                return node.color_scale
 
 
 def _build_graph(
