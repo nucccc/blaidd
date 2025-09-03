ad8dd1585b0c8f2b53af9d8e07e54be38a64d88c
nucccc <dan.dom.nuc@gmail.com>
2025-09-03 13:41:52 +0200

    written code to create scale

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -40,6 +40,25 @@ impl Node {
 }
 
 
+fn create_scale(n_centroids: usize) -> Vec<f64> {
+    // TODO: test this
+    let mut res = Vec::new();
+    res.reserve(n_centroids);
+
+    let step: f64 = 1.0f64 / ((n_centroids-1) as f64);
+
+    for i in 0..(n_centroids-1) {
+        res.push((i as f64) * step);
+    }
+    res.push(1.0);
+
+    res
+}
+
+
+
+
+
 #[pyfunction]
 pub fn abc(
     n_centroids: usize,
@@ -50,14 +69,33 @@ pub fn abc(
 
     let mut graph = Graph::new(n_centroids, &edges);
 
-    /*for elem in l {
-        let boh: PyTuple = elem.extract().unwrap();
-        println!("{boh}");
-    }*/
+    let scale = create_scale(n_centroids);
+
+
 }
 
 #[pymodule]
 fn rblaidd(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(abc, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scale() {
+        let scale0 = create_scale(2);
+        assert_eq!(scale0, vec![0.0, 1.0]);
+
+        let scale1 = create_scale(3);
+        assert_eq!(scale1, vec![0.0, 0.5, 1.0]);
+
+        let scale1 = create_scale(4);
+        assert_eq!(scale1, vec![0.0, 0.3333333333333333, 0.6666666666666666, 1.0]);
+
+        let scale2 = create_scale(5);
+        assert_eq!(scale2, vec![0.0, 0.25, 0.5, 0.75, 1.0]);
+    }
 }
